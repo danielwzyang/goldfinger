c4b1c885433842697bc8c9925596301336fc6737
Daniel Yang <danielwzyang@gmail.com>
2025-05-26 00:17:52 -0400

    cutoff mid alpha beta

--- internal/engine/alphabeta.go
+++ internal/engine/alphabeta.go
@@ -102,6 +102,12 @@ func alphaBeta(ctx context.Context, alpha, beta, depth int) (int, int) {
 	legalMoves := 0
 
 	for moveCount := 0; moveCount < moves.Count; moveCount++ {
+		select {
+		case <-ctx.Done():
+			return 0, 0
+		default:
+		}
+
 		move := moves.Moves[moveCount]
 
 		if !board.MakeMove(move) {
